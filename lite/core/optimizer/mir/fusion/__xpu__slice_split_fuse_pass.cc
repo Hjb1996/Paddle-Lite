77ebb436eeca1e7e5614020582e682f6b7ba2858
gaoyuyang <zdlnlhmj@126.com>
2024-09-10 15:30:34 +0800

    0910 slice

--- lite/core/optimizer/mir/fusion/__xpu__slice_split_fuse_pass.cc
+++ lite/core/optimizer/mir/fusion/__xpu__slice_split_fuse_pass.cc
@@ -89,13 +89,12 @@ class XPUMultiSliceSplitFuser {
           all_slice[i]->stmt()->op_info()->GetAttr<std::vector<int>>("starts")[0] << std::endl;
       }
     }
-
-    auto first_slice = slice_split_sort[0]->stmt()->op();
+    auto first_slice = slice_split_sort[0]->stmt()->op(); // 第一个slice op算子
     auto* scope = first_slice->scope();
     auto& valid_places = first_slice->valid_places();
     std::vector<std::string> out_names;
     std::vector<std::string> in_names;
-    Node* input_node = slice_split_sort[0]->inlinks.front();
+    Node* input_node = slice_split_sort[0]->inlinks.front(); // inlink是结点信息  .front()是数据信息的第一个元素
     std::vector<Node*> output_node;
     std::string in_name = 
         slice_split_sort[0]->stmt()->op_info()->Input("Input").front();
@@ -113,8 +112,7 @@ class XPUMultiSliceSplitFuser {
     GraphSafeRemoveNodes(graph, to_remove);
     // slice
     cpp::OpDesc op_desc;
-    std::string concat_output_name =
-      "__xpu__slice_concat_output";
+    std::string concat_output_name = "__xpu__slice_concat_output"; // 构建一个slice 然后拿concat承接所有slice的结果
     op_desc.SetType("slice");
     op_desc.SetInput("Input", {in_name});
     // op_desc.SetOutput("Out", {concat_output_name});
@@ -127,72 +125,43 @@ class XPUMultiSliceSplitFuser {
         TARGET(kXPU), PRECISION(kFloat));
     scope->NewTensor(concat_output_name);
     op_desc.SetOutput("Out", {concat_output_name});
-    auto new_op = LiteOpRegistry::Global().Create(op_desc.Type());
-    new_op->Attach(op_desc, scope);
-    auto* new_op_node = graph->GraphCreateInstructNode(new_op, valid_places);
+    // 构建op和节点
+    auto slice_new_op = LiteOpRegistry::Global().Create(op_desc.Type());
+    slice_new_op->Attach(op_desc, scope);
+    auto* slice_new_op_node = graph->GraphCreateInstructNode(slice_new_op, valid_places);
 
-    std::string slice4_name = "slice_4.tmp_0";
+    std::string slice4_name = "slice_4.tmp_0"; // 这个是新建的Tensor 这个暂且不表
     auto* slice4_name_node = graph->NewArgumentNode(slice4_name);
     slice4_name_node->arg()->type = LiteType::GetTensorTy(
         TARGET(kXPU), PRECISION(kFloat));
     scope->NewTensor(slice4_name);
-    std::cout << "----148---This is a test for this pass" << std::endl;
 
-    // split新增代码
-    // 定义名字和输出结点名字
-    std::vector<Node*> split_output_node;
-    std::vector<std::string> split_out_node_names;
-    // 输出结点名字
-    for (int i = 0; i < out_names.size(); ++i) {
-      split_out_node_names.push_back("__xpu__split_" + out_names[i]);
-    }
-    auto first_split = new_op_node->stmt()->op();
+    // concat没有对应的outlinks
+    auto first_split = slice_new_op_node->stmt()->op();
     auto* scope_split = first_split->scope();
     cpp::OpDesc split_op_desc;
     split_op_desc.SetType("split");
-    split_op_desc.SetInput("Input", {concat_output_name});
-    split_op_desc.SetAttr<int>("axis", 1);
-    split_op_desc.SetAttr<int>("num", 6);
-    split_op_desc.SetOutput("Out", {split_out_node_names});
-
-    for (int i = 0; i < split_out_node_names.size(); i++) {
-      CHECK(graph->RetrieveArgument(split_out_node_names[i]) == nullptr);
-      std::cout << "out_name:" << split_out_node_names[i] << std::endl;
-      auto* out_name_node = graph->NewArgumentNode(split_out_node_names[i]);
-      out_name_node->arg()->type = LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kFloat));
-      scope_split->NewTensor(split_out_node_names[i]);
-      // auto split_new_op = LiteOpRegistry::Global().Create(split_op_desc.Type());
-      // new_op->Attach(split_op_desc, scope_split);
-      // auto* out_name_node = graph->NewArgumentNode(split_out_node_names[i]);
-      split_output_node.push_back(out_name_node);
-    }  
+    split_op_desc.SetInput("x", {concat_output_name});
+    split_op_desc.SetAttr<int>("axis", {1});
+    split_op_desc.SetAttr<int>("num", {6});
+    for (int i = 0; i < out_names.size(); i++) {
+      if (i == 4) {
+        continue;
+      } else {
+        std::cout << "out_name:" << out_names[i] << std::endl;
+        auto* out_name_node = graph->NewArgumentNode(out_names[i]);
+        out_name_node->arg()->type = LiteType::GetTensorTy(TARGET(kXPU), PRECISION(kFloat));
+        scope_split->NewTensor(out_names[i]);
+      }
+    }
+    split_op_desc.SetOutput("output", {out_names});
     std::cout << "----168---This is a test for this pass" << std::endl;
 
-    DirectedLink(input_node, new_op_node);
-    DirectedLink(new_op_node, concat_output_node);
-    // DirectedLink(concat_output_node, split_node);
-    for (auto& node : split_output_node) {
-      DirectedLink(concat_output_node, node);
+    DirectedLink(input_node, slice_new_op_node);
+    DirectedLink(slice_new_op_node, concat_output_node);
+    for (auto& node : output_node) {
+      DirectedLink(slice_new_op_node, node);
     }
-
-    
-    std::cout << "----157---This is a test for this pass" << std::endl;
-    // 链接
-    int num = 0;
-    for (Node* node : output_node) {
-      std::cout << "num:" << num << std::endl;
-      DirectedLink(split_output_node[num], node);
-      if (num == 3) {
-        num++;
-      }
-      num++;
-    }
-
-    // for (auto& node : new_op_node->outlinks) {
-    //   std::cout << "----167--------:" << std::endl;
-    //   std::cout << "new_op_node:" << *node << std::endl;
-    // }
-
   }
 };
 
